611a6190a8d66ea225ffc373844d1ae4d06f868d
minsukim0207 <126400392+minsukim0207@users.noreply.github.com>
2023-09-04 16:38:53 +0900

    Hard

--- Practice/src/com/kh/practice/chap02/Practice15.java
+++ Practice/src/com/kh/practice/chap02/Practice15.java
@@ -12,16 +12,17 @@ public class Practice15 {
 		System.out.print("숫자를 입력하세요: ");
 		int inputNum = sc.nextInt();
 		
-		if (inputNum > 2) {
-		
-			for (int i = 1; i <= inputNum; i++) {
-				if (inputNum % i == 0) count++;
-				else continue;
+		if (inputNum < 2) {
+			System.out.println("잘못 입력하셨습니다.");		
+		} else {
+			for (int i = 2; i <= inputNum; i++) {
+				for (int j = 2; j <= i; j++) {
+					if (i / j == i) {
+						count = 1;
+					} else continue;
+				}
 			}
-		
-			if (count == 2) System.out.println(inputNum + "은 소수입니다.");
-			else System.out.println(inputNum + "은 소수가 아닙니다.");
-		} else System.out.println("잘못 입력하셨습니다.");
+			if (count == 1) System.out.println(inputNum + "은 소수입니다.");
+		}
 	}
-
 }
