62fa96523cf42d22897edb2227d01093a7fae104
minsukim0207 <126400392+minsukim0207@users.noreply.github.com>
2023-09-12 17:49:06 +0900

    Two Dimensional Array

---
+++ chap21_twodimensionalarray/src/com/kh/arraymain/ArrayMain.java
@@ -0,0 +1,15 @@
+package com.kh.arraymain;
+import com.kh.arrayexam.*;
+
+public class ArrayMain {
+	
+	public static void main(String[] args) {
+		ArrayExam prac = new ArrayExam();
+		
+		//prac.practice1();
+		//prac.practice2();
+		//prac.practice3();
+		prac.practice4();
+	}
+
+}
